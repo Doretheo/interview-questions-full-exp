9fd1e055589dd2d7174988c4904ebd5b76630b0e
Theodore Nguyen <theodorenguyen5@gmail.com>
2021-01-03 19:27:32 -0800

    Organizing space for problems

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path')
+let db = require('../database');
 
 let port = 2440;
 let app = express();
--- src/components/Problems.jsx
+++ src/components/Problems.jsx
@@ -2,24 +2,42 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
+const Grid = styled.div``;
+
+const Row  = styled.div`
+  display: flex;
+`;
+
+const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
+
 class Problems extends Component {
   constructor() {
     super();
     this.state = {
-
+      data: null
     };
     this.grabProblems = this.grabProblems.bind(this);
   }
 
   grabProblems() {
     axios.get('problems')
-      .then(res => res.data)
+      .then(res => this.setState({
+        data: res.data
+      }))
       .catch(err => console.log(err))
   }
 
   render() {
+    const { data } = this.state;
     return (
-      <p>Problem sets</p>
+      <div>
+        <p>Problem sets</p>
+        {/* {data.map((p) => (
+          <p>{p}</p>
+        ))} */}
+      </div>
     )
   }
 }
